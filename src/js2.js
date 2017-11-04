2dd6c98874d242534b58d7bde8b80d4d729799cd
thorgal333 <francedigitale@gmx.fr>
2017-11-04 16:00:09 +0100

    Version2

--- src/js2.js
+++ src/js2.js
@@ -2,6 +2,6 @@ import {display} from './js1.js';
 import image from "./Architecture1.jpg";
 import "./page.css";
 document.getElementById("image").innerHTML = '<img src="dist/' + image + '" alt="photo">';
-
+  
 
 //display("hello world");
